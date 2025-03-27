96c0bfcd4086ab8de338f8dcb5aad567c9aadcfc
Briyomal <briyomal@gmail.com>
2025-03-27 23:23:51 +0530

    Index modify

--- backend/index.js
+++ backend/index.js
@@ -62,12 +62,22 @@ app.use("/api/profile", profileRoutes);
 // Move one level up from `backend`
 const rootDir = path.resolve(__dirname, "..");  
 
+/*
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(rootDir, "frontend/dist")));
 	app.get("*", (req, res) =>
 		res.sendFile(path.resolve(rootDir, "frontend", "dist", "index.html"))
 	);
 }
+	*/
+
+	if (process.env.NODE_ENV === "production") {
+		app.use(express.static(path.join(__dirname, "..", "frontend", "dist")));
+		app.get("*", (req, res) => 
+		  res.sendFile(path.resolve(__dirname, "..", "frontend", "dist", "index.html"))
+		);
+	  }
+	  
 
 /*
 app.listen(PORT, () => {
