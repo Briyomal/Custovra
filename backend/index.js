100b57bafd74dfa4574cfd3ce3e494abeaa4350a
Briyomal <briyomal@gmail.com>
2025-03-28 11:50:07 +0530

    Add api folder

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,24 @@
 import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import path from "path";
+
+import { connectDB } from "./db/connectDB.js";
+import apiRoutes from "./api/index.js"; // Import API routes
+
+dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", apiRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server is running on port:", PORT);
 });
-
-// Export app instead of listening on a port
-export default app;
