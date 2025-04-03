9eeb289e752687d7a86b1e606988bc1271cbab6a
Briyomal <briyomal@gmail.com>
2025-04-03 11:17:44 +0530

    Vercel backend URL Fix api error

--- frontend/src/store/formStore.js
+++ frontend/src/store/formStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.MODE === "development" 
     ? "http://localhost:5000/api/forms" 
-    : "https://review-app-briyomals-projects.vercel.app";
+    : "https://review-app-briyomals-projects.vercel.app/api/forms";
 
 axios.defaults.withCredentials = true;
 
--- frontend/src/store/profileStore.js
+++ frontend/src/store/profileStore.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const API_URL = import.meta.env.MODE === "development" 
     ? "http://localhost:5000/api/profile" 
-    : "https://review-app-briyomals-projects.vercel.app";
+    : "https://review-app-briyomals-projects.vercel.app/api/profile";
 
 axios.defaults.withCredentials = true;
 
