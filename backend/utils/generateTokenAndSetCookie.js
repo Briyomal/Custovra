0864c83c853d3b009e62f094452fe785fddf772a
Briyomal <briyomal@gmail.com>
2025-04-06 12:58:25 +0530

    JWT Token Strict None and Secure True

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -7,8 +7,9 @@ export const generateTokenAndSetCookie = (res, userId) => {
 
 	res.cookie("token", token, {
 		httpOnly: true,
-		secure: process.env.NODE_ENV === "production",
-		sameSite: "strict",
+		secure: true,
+		//secure: process.env.NODE_ENV === "production",
+		sameSite: "none",
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 
