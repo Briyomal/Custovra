14e799b8c03c2538d8bccd80129769094f903ece
Briyomal <briyomal@gmail.com>
2025-11-26 01:09:45 +0530

    Email sending

--- backend/email/smtp.config.js
+++ backend/email/smtp.config.js
@@ -6,13 +6,13 @@ dotenv.config();
 
 // Replace these with your actual SMTP server details
 export const transporter = nodemailer.createTransport({
-	host: "ax.email", // e.g., mail.yourdomain.com
+	host: "smtp.zoho.com", // e.g., mail.yourdomain.com
 	port: 465, // or 587 depending on your server
 	secure: true, // true for port 465, false for 587
 	auth: {
-		user: "webforms@alphaclick.biz",
-		pass: "4Jj1]|2[",
+		user: "noreply@custovra.com",
+		pass: "Vssqlx7-",
 	},
 });
 
-export const sender = `"Review Platform" <webforms@alphaclick.biz>`;
+export const sender = `"Custovra" <noreply@custovra.com>`;
